88641811c81be0d04cfb6812e9315bd3b3d36b8a
L200180192-new <55448128+L200180192-new@users.noreply.github.com>
2020-03-10 21:22:23 +0700

    Add files via upload

---
+++ L200180192_Modul2_G/Riskamp.py
@@ -0,0 +1,353 @@
+# nomer1
+
+class Pesan(object):
+    """Sebuah class bernama Pesan.
+       Untuk memahami konsep Class dan Object.
+    """
+    def __init__(self, sebuahString):
+        self.teks = sebuahString
+    def cetakIni(self):
+        print(self.teks)
+    def cetakPakaiHurufKapital(self):
+        print(str.upper(self.teks))
+    def cetakPakaiHurufKecil(self):
+        print(str.lower(self.teks))
+    def jumKar(self):
+        return len(self.teks)
+    def cetakJumlahKarakterku(self):
+        print('kalimatku mempunyai', len(self.teks), 'karakter.')
+    def perbarui(self, stringBaru):
+        self.teks = stringBaru
+
+#a
+    def apakahTerkandung(self, a):
+        if a in self.teks:
+            return True
+        else :
+            return False
+#b
+    def hitungKonsonan(self):
+        kon = 0
+        x = self.teks
+        alfaKon = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"
+        for i in x:
+            if i in alfaKon:
+                kon += 1
+        return kon
+#c
+    def hitungVokal(self):
+        voc = 0
+        x = self.teks
+        alfaVoc = "aiueoAIUEO"
+        for i in x:
+            if i in alfaVoc:
+                voc += 1
+        return voc
+
+# nomer2
+
+class Manusia(object):
+    """class 'Manusia' dengan inisiasi 'nama'"""
+    keadaan = "lapar"
+    def __init__(self, nama):
+        self.nama = nama
+    def ucapkanSalam(self):
+        print("Salam, namaku ",self.nama)
+    def makan(self, s):
+        print("Saya baru saja makan ", s)
+        self.keadaan = 'kenyang'
+    def olahraga(self, k):
+        print("Saya baru saja latihan ", k)
+        self.keadaan = 'lapar'
+    def mengalikanDenganDua(self, n):
+        return n * 2
+
+class Mahasiswa(Manusia):
+    """Class Mahasiswa yang dibangun dari class Manusia."""
+    def __init__(self, nama, NIM, kota, us):
+        """Metode inisiasi ini menutupi metode inisiasi di class Manusia"""
+        self.nama = nama
+        self.NIM = NIM
+        self.kotaTinggal = kota
+        self.uangSaku = us
+    def __str__(self):
+        s = self.nama + ', NIM ' + str(self.NIM) \
+            + '. Tinggal di ' + self.kotaTinggal \
+            + ' tiap bulannya.'
+        return s
+    def ambilNama(self):
+        return self.nama
+    def ambilNIM(self):
+        return self.NIM
+    def ambilUangSaku(self):
+        return self.uangSaku
+    def makan(self, s):
+        """Metode ini menutupi metode 'makan'-nya class manusia.
+           Mahasiswa kalau makan sambil belajar."""
+        print("Saya baru saja makan", s, "Sambil belajar.")
+        self.keadaan = 'kenyang'
+#a
+    def ambilKotaTinggal(self):
+        return self.kotaTinggal
+#b
+    def perbaruiKotaTinggal(self, ubah):
+        self.kotaTinggal = ubah
+#c
+    def TambahUangSaku(self, tambah):
+        self.uangSaku += tambah
+
+# nomer3
+
+class Manusia(object):
+    """class 'Manusia' dengan inisiasi 'nama'"""
+    keadaan = "lapar"
+    def __init__(self, nama):
+        self.nama = nama
+    def ucapkanSalam(self):
+        print("Salam, namaku ",self.nama)
+    def makan(self, s):
+        print("Saya baru saja makan ", s)
+        self.keadaan = 'kenyang'
+    def olahraga(self, k):
+        print("Saya baru saja latihan ", k)
+        self.keadaan = 'lapar'
+    def mengalikanDenganDua(self, n):
+        return n * 2
+
+class Mahasiswa(Manusia):
+    """Class Mahasiswa yang dibangun dari class Manusia."""
+    def __init__(self, nama, NIM, kota, us):
+        """Metode inisiasi ini menutupi metode inisiasi di class Manusia"""
+        self.nama = nama
+        
+        self.NIM = NIM
+        self.kotaTinggal = kota
+        self.uangSaku = us
+    def __str__(self):
+        s = self.nama + ', NIM ' + str(self.NIM) \
+            + '. Tinggal di ' + self.kotaTinggal \
+            + ' tiap bulannya.'
+        return s
+    def ambilNama(self):
+        return self.nama
+    def ambilNIM(self):
+        return self.NIM
+    def ambilUangSaku(self):
+        return self.uangSaku
+    def makan(self, s):
+        """Metode ini menutupi metode 'makan'-nya class manusia.
+           Mahasiswa kalau makan sambil belajar."""
+        print("Saya baru saja makan", s, "Sambil belajar.")
+        self.keadaan = 'kenyang'
+
+    def ambilKotaTinggal(self):
+        return self.kotaTinggal
+    def perbaruiKotaTinggal(self, ubah):
+        self.kotaTinggal = ubah
+    def tambahUangSaku(self, tambah):
+        self.uangSaku += tambah
+
+print("Masukkan Data Mahasiswa Disini :")
+a = input("Nama      : ")
+b = input("NIM       : ")
+c = input("Asal      : ")
+d = input("Uang Saku : ")
+maha = Mahasiswa(a, b, c, d)
+print("""Silahkan Ketik 'maha.instruksi' untuk menjalankan program yang
+kalian inginkan.""")
+
+# nomer4
+
+class Manusia(object):
+    """class 'Manusia' dengan inisiasi 'nama'"""
+    keadaan = "lapar"
+    def __init__(self, nama):
+        self.nama = nama
+    def ucapkanSalam(self):
+        print("Salam, namaku ",self.nama)
+    def makan(self, s):
+        print("Saya baru saja makan ", s)
+        self.keadaan = 'kenyang'
+    def olahraga(self, k):
+        print("Saya baru saja latihan ", k)
+        self.keadaan = 'lapar'
+    def mengalikanDenganDua(self, n):
+        return n * 2
+
+class Mahasiswa(Manusia):
+    """Class Mahasiswa yang dibangun dari class Manusia."""
+    def __init__(self, nama, NIM, kota, us):
+        """Metode inisiasi ini menutupi metode inisiasi di class Manusia"""
+        self.nama = nama
+        self.NIM = NIM
+        self.kotaTinggal = kota
+        self.uangSaku = us
+    def __str__(self):
+        s = self.nama + ', NIM ' + str(self.NIM) \
+            + '. Tinggal di ' + self.kotaTinggal \
+            + ' tiap bulannya.'
+        return s
+    def ambilNama(self):
+        return self.nama
+    def ambilNIM(self):
+        return self.NIM
+    def ambilUangSaku(self):
+        return self.uangSaku
+    def makan(self, s):
+        """Metode ini menutupi metode 'makan'-nya class manusia.
+           Mahasiswa kalau makan sambil belajar."""
+        print("Saya baru saja makan", s, "Sambil belajar.")
+        self.keadaan = 'kenyang'
+
+    def ambilKotaTinggal(self):
+        return self.kotaTinggal
+    def perbaruiKotaTinggal(self, ubah):
+        self.kotaTinggal = ubah
+    def tambahUangSaku(self, tambah):
+        self.uangSaku += tambah
+
+    listKuliah = []
+    def ambilKuliah(self, kuliah):
+        self.listKuliah.append(kuliah)
+
+# nomer5
+
+class Manusia(object):
+    """class 'Manusia' dengan inisiasi 'nama'"""
+    keadaan = "lapar"
+    def __init__(self, nama):
+        self.nama = nama
+    def ucapkanSalam(self):
+        print("Salam, namaku ",self.nama)
+    def makan(self, s):
+        print("Saya baru saja makan ", s)
+        self.keadaan = 'kenyang'
+    def olahraga(self, k):
+        print("Saya baru saja latihan ", k)
+        self.keadaan = 'lapar'
+    def mengalikanDenganDua(self, n):
+        return n * 2
+
+class Mahasiswa(Manusia):
+    """Class Mahasiswa yang dibangun dari class Manusia."""
+    def __init__(self, nama, NIM, kota, us):
+        """Metode inisiasi ini menutupi metode inisiasi di class Manusia"""
+        self.nama = nama
+        self.NIM = NIM
+        self.kotaTinggal = kota
+        self.uangSaku = us
+    def __str__(self):
+        s = self.nama + ', NIM ' + str(self.NIM) \
+            + '. Tinggal di ' + self.kotaTinggal \
+            + ' tiap bulannya.'
+        return s
+    def ambilNama(self):
+        return self.nama
+    def ambilNIM(self):
+        return self.NIM
+    def ambilUangSaku(self):
+        return self.uangSaku
+    def makan(self, s):
+        """Metode ini menutupi metode 'makan'-nya class manusia.
+           Mahasiswa kalau makan sambil belajar."""
+        print("Saya baru saja makan", s, "Sambil belajar.")
+        self.keadaan = 'kenyang'
+
+    def ambilKotaTinggal(self):
+        return self.kotaTinggal
+    def perbaruiKotaTinggal(self, ubah):
+        self.kotaTinggal = ubah
+    def tambahUangSaku(self, tambah):
+        self.uangSaku += tambah
+
+    listKuliah = []
+    def ambilKuliah(self, kuliah):
+        self.listKuliah.append(kuliah)
+    def hapusKuliah(self, hapus):
+        self.listKuliah.remove(hapus)
+
+# nomer6
+
+class Manusia(object):
+    """class 'Manusia' dengan inisiasi 'nama'"""
+    keadaan = "lapar"
+    def __init__(self, nama):
+        self.nama = nama
+    def ucapkanSalam(self):
+        print("Salam, namaku ",self.nama)
+    def makan(self, s):
+        print("Saya baru saja makan ", s)
+        self.keadaan = 'kenyang'
+    def olahraga(self, k):
+        print("Saya baru saja latihan ", k)
+        self.keadaan = 'lapar'
+    def mengalikanDenganDua(self, n):
+        return n * 2
+
+class Mahasiswa(Manusia):
+    """Class Mahasiswa yang dibangun dari class Manusia."""
+    def __init__(self, nama, NIM, kota, us):
+        """Metode inisiasi ini menutupi metode inisiasi di class Manusia"""
+        self.nama = nama
+        self.NIM = NIM
+        self.kotaTinggal = kota
+        self.uangSaku = us
+    def __str__(self):
+        s = self.nama + ', NIM ' + str(self.NIM) \
+            + '. Tinggal di ' + self.kotaTinggal \
+            + ' tiap bulannya.'
+        return s
+    def ambilNama(self):
+        return self.nama
+    def ambilNIM(self):
+        return self.NIM
+    def ambilUangSaku(self):
+        return self.uangSaku
+    def makan(self, s):
+        """Metode ini menutupi metode 'makan'-nya class manusia.
+           Mahasiswa kalau makan sambil belajar."""
+        print("Saya baru saja makan", s, "Sambil belajar.")
+        self.keadaan = 'kenyang'
+
+    def ambilKotaTinggal(self):
+        return self.kotaTinggal
+    def perbaruiKotaTinggal(self, ubah):
+        self.kotaTinggal = ubah
+    def tambahUangSaku(self, tambah):
+        self.uangSaku += tambah
+
+    listKuliah = []
+    def ambilKuliah(self, kuliah):
+        self.listKuliah.append(kuliah)
+    def hapusKuliah(self, hapus):
+        self.listKuliah.remove(hapus)
+
+class SiswaSMA(Manusia):
+    def __init__(self, nama, NISN, uangSaku, alamat):
+        self.nama = nama
+        self.nisn = NISN
+        self.uangSaku = uangSaku
+        self.alamat = alamat
+    def __str__(self):
+        a = 'Nama      : ' + str(self.nama) + '\n' \
+            + 'NISN      : ' + str(self.nisn) + '\n' \
+            + 'Alamat    : ' + str(self.alamat) + '\n' \
+            + 'Uang Saku : ' + str(self.uangSaku)
+        return a
+
+# nomer7
+
+import no2_modul2         # Atau apapun file-nya yang kamu buat tadi
+
+class MhsTIF(no2_modul2.Mahasiswa):    # perhatikan class induknya : Mahasiswa
+    """Class MhsTIF yang dibangun dari class Mahasiswa"""
+    def kataKanPy(self):
+        print('Python is cool.')
+
+# Apakah metode / state itu berasal dari class Manusia, Mahasiswa, atau MhsTIF?
+
+# Jawab :
+
+# Metoode atau state yang muncul berasal dari semua class baik Manusia, Mahasiswa, atau MhsTIF.
+# Ini karena MhsTIF yang merupakan anak class dari Mahasiswa, dan itu membuat MhsTIF mewarisi
+# semua properties dari Mahasiswa dan Manusia.
